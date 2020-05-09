4385b64efca71b7f66df8dfdd6c14a9412f19aac
Herve Saint-Amand <git@saintamh.prg>
2020-05-09 19:28:34 +0100

    Refactor tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -33,258 +33,263 @@ class AstClass:
 AST = AstClass()
 
 
+class Case(NamedTuple):
+    input_text: str
+    expected_parse: AstNode
+
+
 @pytest.mark.parametrize(
-    'input_text, expected',
+    ','.join(Case._fields),
     [
 
         # Numbers
-        (
+        Case(
             '42',
-            AST.int('42'),
+            expected_parse=AST.int('42'),
         ),
-        (
+        Case(
             '3.14',
-            AST.float('3.14'),
+            expected_parse=AST.float('3.14'),
         ),
-        (
+        Case(
             '5.12e-1',
-            AST.float('5.12e-1'),
+            expected_parse=AST.float('5.12e-1'),
         ),
 
         # Named constants
-        (
+        Case(
             'true',
-            AST.named_constant(True),
+            expected_parse=AST.named_constant(True),
         ),
-        (
+        Case(
             'false',
-            AST.named_constant(False),
+            expected_parse=AST.named_constant(False),
         ),
-        (
+        Case(
             'null',
-            AST.named_constant(None),
+            expected_parse=AST.named_constant(None),
         ),
-        (
+        Case(
             'undefined',
-            AST.named_constant({'$undefined': True}),
+            expected_parse=AST.named_constant({'$undefined': True}),
         ),
-        (
+        Case(
             'NotImplemented',
-            AST.named_constant('NotImplemented'),
+            expected_parse=AST.named_constant('NotImplemented'),
         ),
 
         # Arrays
-        (
+        Case(
             '[1]',
-            AST.array([AST.int('1')]),
+            expected_parse=AST.array([AST.int('1')]),
         ),
-        (
+        Case(
             '[1,]',
-            AST.array([AST.int('1')]),
+            expected_parse=AST.array([AST.int('1')]),
         ),
-        (
+        Case(
             '[,]',
-            ParserError("Expected expression, found ',]'"),
+            expected_parse=ParserError("Expected expression, found ',]'"),
         ),
-        (
+        Case(
             '[1,,]',
-            ParserError("Expected expression, found ',]'"),
+            expected_parse=ParserError("Expected expression, found ',]'"),
         ),
-        (
+        Case(
             '[,1]',
-            ParserError("Expected expression, found ',1]'"),
+            expected_parse=ParserError("Expected expression, found ',1]'"),
         ),
-        (
+        Case(
             '[1, ["a"]]',
-            AST.array([AST.int('1'), AST.array([AST.string('a')])]),
+            expected_parse=AST.array([AST.int('1'), AST.array([AST.string('a')])]),
         ),
 
         # Tuples
-        (
+        Case(
             '(1,)',
-            AST.array([AST.int('1')]),
+            expected_parse=AST.array([AST.int('1')]),
         ),
-        (
+        Case(
             '(1)',
-            ParserError("Expected /,/, found ')'"),
+            expected_parse=ParserError("Expected /,/, found ')'"),
         ),
-        (
+        Case(
             '(,)',
-            ParserError("Expected expression, found ',)'"),
+            expected_parse=ParserError("Expected expression, found ',)'"),
         ),
-        (
+        Case(
             '(1,,)',
-            ParserError("Expected expression, found ',)'"),
+            expected_parse=ParserError("Expected expression, found ',)'"),
         ),
-        (
+        Case(
             '(,1)',
-            ParserError("Expected expression, found ',1)'"),
+            expected_parse=ParserError("Expected expression, found ',1)'"),
         ),
-        (
+        Case(
             '(1, ("a",))',
-            AST.array([AST.int('1'), AST.array([AST.string('a')])]),
+            expected_parse=AST.array([AST.int('1'), AST.array([AST.string('a')])]),
         ),
 
         # Mappings
-        (
+        Case(
             '{"a": 1}',
-            AST.mapping({AST.string('a'): AST.int('1')}),
+            expected_parse=AST.mapping({AST.string('a'): AST.int('1')}),
         ),
-        (
+        Case(
             '{"a": 1,}',
-            AST.mapping({AST.string('a'): AST.int('1')}),
+            expected_parse=AST.mapping({AST.string('a'): AST.int('1')}),
         ),
-        (
+        Case(
             '{"a": 1,,}',
-            ParserError("Expected key, found ',}'"),
+            expected_parse=ParserError("Expected key, found ',}'"),
         ),
-        (
+        Case(
             '{,"a": 1}',
-            ParserError('Expected key, found \',"a": 1}\''),
+            expected_parse=ParserError('Expected key, found \',"a": 1}\''),
         ),
-        (
+        Case(
             '{a: 1}',
-            AST.mapping({AST.identifier('a'): AST.int('1')}),
+            expected_parse=AST.mapping({AST.identifier('a'): AST.int('1')}),
         ),
 
         # Sets
-        (
+        Case(
             '{1}',
-            AST.set([AST.int('1')]),
+            expected_parse=AST.set([AST.int('1')]),
         ),
-        (
+        Case(
             '{1,}',
-            AST.set([AST.int('1')]),
+            expected_parse=AST.set([AST.int('1')]),
         ),
-        (
+        Case(
             '{1,,}',
-            ParserError("Expected expression, found ',}'"),
+            expected_parse=ParserError("Expected expression, found ',}'"),
         ),
-        (
+        Case(
             '{,1}',
-            ParserError("Expected key, found ',1}'"),
+            expected_parse=ParserError("Expected key, found ',1}'"),
         ),
-        (
+        Case(
             '{1, 2}',
-            AST.set([AST.int('1'), AST.int('2')]),
+            expected_parse=AST.set([AST.int('1'), AST.int('2')]),
         ),
 
         # Single-quoted strings
-        (
+        Case(
             '''
                 'text'
             ''',
-            AST.string('text'),
+            expected_parse=AST.string('text'),
         ),
-        (
+        Case(
             r'''
                 'I "love\" this'
             ''',
-            AST.string('I "love" this'),
+            expected_parse=AST.string('I "love" this'),
         ),
-        (
+        Case(
             r'''
                 'It\'s what\'s most'
             ''',
-            AST.string("It's what's most"),
+            expected_parse=AST.string("It's what's most"),
         ),
-        (
+        Case(
             # NB input is raw string (i.e. the string includes the backslashes), output is not (so is parsed as normal Python
-            # strinig literals are)
+            # string literals are)
             r'''
                 ' \\ \" \/ \b \f \n \r \t \u0424 \x7E \' '
             ''',
-            AST.string(' \\ " / \x08 \x0C \n \r \t Ф ~ \' '),
+            expected_parse=AST.string(' \\ " / \x08 \x0C \n \r \t Ф ~ \' '),
         ),
 
         # Double-quoted strings
-        (
+        Case(
             '''
                 "text"
             ''',
-            AST.string('text'),
+            expected_parse=AST.string('text'),
         ),
-        (
+        Case(
             r'''
                 "I \"love\" this"
             ''',
-            AST.string('I "love" this'),
+            expected_parse=AST.string('I "love" this'),
         ),
-        (
+        Case(
             r'''
                 "It's what\'s most"
             ''',
-            AST.string("It's what's most"),
+            expected_parse=AST.string("It's what's most"),
         ),
-        (
+        Case(
             # NB input is raw string (i.e. the string includes the backslashes), output is not (so is parsed as normal Python
-            # strinig literals are)
+            # string literals are)
             r'''
                 " \\ \" \/ \b \f \n \r \t \u0424 \x7E \' "
             ''',
-            AST.string(' \\ " / \x08 \x0C \n \r \t Ф ~ \' '),
+            expected_parse=AST.string(' \\ " / \x08 \x0C \n \r \t Ф ~ \' '),
         ),
 
         # Comments
-        (
+        Case(
             '''
                 36 // thirty-six
             ''',
-            AST.int('36'),
+            expected_parse=AST.int('36'),
         ),
-        (
+        Case(
             '''
                // a comment
                "http://not.a.comment/"
             ''',
-            AST.string('http://not.a.comment/'),
+            expected_parse=AST.string('http://not.a.comment/'),
         ),
 
         # Python repr expressions
-        (
+        Case(
             '<__main__.C object at 0x05b0ace99cf7>',
-            AST.python_repr('<__main__.C object at 0x05b0ace99cf7>'),
+            expected_parse=AST.python_repr('<__main__.C object at 0x05b0ace99cf7>'),
         ),
-        (
+        Case(
             "<re.Match object; span=(0, 1), match='a'>",
-            AST.python_repr("<re.Match object; span=(0, 1), match='a'>"),
+            expected_parse=AST.python_repr("<re.Match object; span=(0, 1), match='a'>"),
         ),
-        (
+        Case(
             "<dummy match='<>'>",
-            AST.python_repr("<dummy match='<>'>"),
+            expected_parse=AST.python_repr("<dummy match='<>'>"),
         ),
 
         # Identifiers
-        (
+        Case(
             'x',
-            AST.identifier('x'),
+            expected_parse=AST.identifier('x'),
         ),
-        (
+        Case(
             'x10',
-            AST.identifier('x10'),
+            expected_parse=AST.identifier('x10'),
         ),
-        (
+        Case(
             'μεταφορά',
-            AST.identifier('μεταφορά'),
+            expected_parse=AST.identifier('μεταφορά'),
         ),
 
         # Function calls
-        (
+        Case(
             'x(1)',
-            AST.function_call('x', [AST.int('1')]),
+            expected_parse=AST.function_call('x', [AST.int('1')]),
         ),
-        (
+        Case(
             'x(1,)',
-            AST.function_call('x', [AST.int('1')]),
+            expected_parse=AST.function_call('x', [AST.int('1')]),
         ),
-        (
+        Case(
             'x(1, 2)',
-            AST.function_call('x', [AST.int('1'), AST.int('2')]),
+            expected_parse=AST.function_call('x', [AST.int('1'), AST.int('2')]),
         ),
 
     ]
 )
-def test_parser(input_text, expected):
+def test_parser(input_text, expected_parse):
     lexer = Lexer(input_text)
     mock_builder = AstClass()
     parser = Parser(lexer, mock_builder)
@@ -292,9 +297,9 @@ def test_parser(input_text, expected):
         document = parser.document()
         actual_parse, = document.values
     except ParserError as actual_error:
-        if isinstance(expected, ParserError):
-            assert str(actual_error) == str(expected)
+        if isinstance(expected_parse, ParserError):
+            assert str(actual_error) == str(expected_parse)
         else:
             raise
     else:
-        assert actual_parse == expected
+        assert actual_parse == expected_parse
